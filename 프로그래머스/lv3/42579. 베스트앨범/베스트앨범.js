60419efc119534752d3d40d0b4afa11cbdb28b1d
crowwan <32172056+crowwan@users.noreply.github.com>
2023-01-25 22:48:10 +0900

    [level 3] Title: 베스트앨범, Time: 0.20 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.js
@@ -1,27 +1,25 @@
 function solution(genres, plays) {
     const map = genres.map((a,i) => ({genre:a, play:plays[i], ind: i})).reduce((a,b) => {
-        
         !a[b.genre] && (a[b.genre] = {});
+        
         !a[b.genre].li && (a[b.genre].li = []);
         a[b.genre].li.push(b);
+        
         !a[b.genre].sum && (a[b.genre].sum = 0);
         a[b.genre].sum += b.play;
+        
         return a;
     },{});
-    console.log(map);
+    
     Object.values(map).forEach(a =>{
         a.li.sort((a,b) => b.play - a.play || a.i - b.i);
         a.li.length > 2 && (a.li.length = 2);
     });
     
-    const sortedKey = Object.keys(map).sort((a,b) => {
-        return map[b].sum - map[a].sum;
-    });
-    console.log(sortedKey)
+    const sortedKey = Object.keys(map).sort((a,b) => map[b].sum - map[a].sum);
+
     
     const res = [];
-    sortedKey.forEach(a =>{
-        map[a].li.forEach(b => res.push(b.ind));
-    })
+    sortedKey.forEach(a =>map[a].li.forEach(b => res.push(b.ind)))
     return res;
 }
