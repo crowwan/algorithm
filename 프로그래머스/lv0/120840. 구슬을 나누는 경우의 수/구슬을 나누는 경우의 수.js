01c3f13a92af91f919f03ba5e24b79d205880c33
crowwan <32172056+crowwan@users.noreply.github.com>
2023-01-26 17:51:31 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.js
+++ 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.js
@@ -1,9 +1,8 @@
 function solution(balls, share) {
-    if(balls === share) return 1;
-    return factorial(balls) / (factorial((balls - share)) * factorial(share))
+    return Math.round(factorial(balls) / (factorial((balls - share)) * factorial(share)))
 }
 
 function factorial(num){
-    if(num === 1) return BigInt(num);
-    return BigInt(num) * factorial(num-1);
+    if(num <= 1) return 1;
+    return num * factorial(num-1);
 }
