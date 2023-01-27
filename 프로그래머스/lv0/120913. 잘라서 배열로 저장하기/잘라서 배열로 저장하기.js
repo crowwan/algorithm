17625d53ac1975920534578c49f02f3665ede9b1
crowwan <32172056+crowwan@users.noreply.github.com>
2023-01-27 18:28:11 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 3.66 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
@@ -0,0 +1,8 @@
+function solution(my_str, n) {
+    const res = [];
+    for(let i = 0;i < my_str.length; i+=n){
+        res.push(my_str.slice(i,i+n));
+    }
+    console.log(res);
+    return res;
+}
