a9a3ba56e5ad4cc707c5078bedff1b7c65a2a56d
crowwan <32172056+crowwan@users.noreply.github.com>
2023-02-02 19:45:41 +0900

    [unrated] Title: 둘만의 암호, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.js
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.js
@@ -1,11 +1,9 @@
 function solution(s, skip, index) {
     const alphabet = [...'abcdefghijklmnopqrstuvwxyz'];
     const filtered = alphabet.filter(a => !skip.includes(a));
-    const res = [...s].map(a => {
-        const ind = filtered.findIndex(c => c === a);
-        console.log( (ind + index) , (filtered.length))
-        return filtered[(ind + index) % filtered.length];
-    });
     
-    return res.join('');
+    return [...s].map(a => {
+        const ind = filtered.findIndex(c => c === a);
+        return filtered[(ind + index) % filtered.length];
+    }).join('');
 }
