4641d952fb134ad75cdf5e6190d71e25dffc71b7
Jinwan Kim <32172056+crowwan@users.noreply.github.com>
2023-03-02 00:00:49 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,3 +1,5 @@
 function solution(phone_number) {
     return [...phone_number.slice(0,-4)].map(_ => '*').join('') + phone_number.slice(-4)
 }
+
+// const solution = n => [...n].fill("*",0,n.length-4).join("")
