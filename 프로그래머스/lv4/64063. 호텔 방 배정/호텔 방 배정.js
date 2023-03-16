00d0edb843865f8f34dc381230e743045e16985d
Jinwan Kim <32172056+crowwan@users.noreply.github.com>
2023-03-16 17:56:41 +0900

    [level 4] Title: 호텔 방 배정, Time: 212.52 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/lv4/64063. 호텔 방 배정/호텔 방 배정.js
+++ 프로그래머스/lv4/64063. 호텔 방 배정/호텔 방 배정.js
@@ -1,20 +1,36 @@
+// function solution(k, room_number) {
+//     const roomMap = new Map();
+    
+//     return room_number.map((a) => {
+//         if(!roomMap.get(a)) {
+//             roomMap.set(a,a+1);
+//             return a;
+//         }
+//         else{
+//             let next = roomMap.get(a);
+//             while(roomMap.get(next)){
+//                 roomMap.get(roomMap.get(next)) && roomMap.set(next,roomMap.get(roomMap.get(next)));
+//                 next = roomMap.get(next); 
+//             }
+//             roomMap.set(a,next + 1);
+//             roomMap.set(next, next + 1);
+//             return next;
+//         }
+//     });
+// }
+
+const getRoomNumber = (map, n) => {
+    if(!map.get(n)){
+        map.set(n, n+1);
+        return n;
+    }
+    map.set(n, getRoomNumber(map,map.get(n)));
+    return map.get(n);
+}
+
 function solution(k, room_number) {
     const roomMap = new Map();
     
-    return room_number.map((a) => {
-        if(!roomMap.get(a)) {
-            roomMap.set(a,a+1);
-            return a;
-        }
-        else{
-            let next = roomMap.get(a);
-            while(roomMap.get(next)){
-                roomMap.get(roomMap.get(next)) && roomMap.set(next,roomMap.get(roomMap.get(next)));
-                next = roomMap.get(next); 
-            }
-            roomMap.set(a,next + 1);
-            roomMap.set(next, next + 1);
-            return next;
-        }
-    });
+    return room_number.map(a => getRoomNumber(roomMap,a));
 }
+
