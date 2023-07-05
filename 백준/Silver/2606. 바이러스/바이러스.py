c7213b2620fab079233e46f13c588a27c8549141
Jinwan Kim <32172056+crowwan@users.noreply.github.com>
2023-07-05 11:31:06 +0900

    [Silver III] Title: 바이러스, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,20 +1,25 @@
-import sys
-input = sys.stdin.readline
-
 n = int(input())
-computers = list(range(0, n+1))
+m = int(input())
 
-def find(u):
-    if computers[u] == u:
-        return u # 자기 자신이 되는 순간 그것이 root이니 그것을 return 하는 것이다.
-    computers[u] = find(computers[u])
-    return computers[u]
+NodeList = [i for i in range(n+1)]
 
-def union(x, y):
-    computers[find(y)] = find(x)
+def find(x):
+    if(NodeList[x] == x):
+        return x
+    else:
+        NodeList[x] = find(NodeList[x])
+        return NodeList[x]
 
-for _ in range(int(input())):
-    i, j = map(int, input().split())
-    union(i, j)
+def union(x,y):
+    x = find(x)
+    y = find(y)
+    
+    if x != y:
+        NodeList[y] = x
 
+
+for _ in range(m):
+    a,b = map(int,input().split())
+    
+    union(a,b)
 print(sum(1 for i in range(2, n+1) if find(1)==find(i)))
